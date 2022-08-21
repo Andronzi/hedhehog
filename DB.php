316fadd32b6e56d495186b56b5ba973d1a881e8e
Andronzi <andrey.radioniv.2003@gmail.com>
2022-08-21 14:55:28 +0500

    create DB class, conn method

--- DB.php
+++ DB.php
@@ -5,10 +5,10 @@ class DB {
     private $port = 3306;
     private $dbname = "hedgehog";
     private $username = 'root';
-    private $password = '1234';
+    private $password = 'root';
 
     public function createPDO() {
-        $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->dbname;charset=utf-8";
+        $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->dbname;charset=utf8";
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -18,8 +18,9 @@ class DB {
 
         try {
             $pdo =  new PDO($dsn, $this->username, $this->password, $options);
-            echo "Подключение к $this->dbname прошло успешно";
+            echo "Подключение к $this->dbname прошло успешно" . "\n";
         } catch (PDOException $error) {
+            http_response_code(500);
             echo $error->getMessage();
         }
 
