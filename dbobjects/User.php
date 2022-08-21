be18fd6e0d9b77265c6bf35d3ed944bbd671a949
Andronzi <andrey.radioniv.2003@gmail.com>
2022-08-21 22:15:47 +0500

    add getting users in User

--- dbobjects/User.php
+++ dbobjects/User.php
@@ -8,22 +8,19 @@ class User
     public $username;
     public $password;
 
-    public function __construct($name, $surname, $username, $password) {
-        $this->name = $name;
-        $this->surname = $surname;
-        $this->username = $username;
-        $this->password = $password;
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
     }
 
-    public function createUser($pdo) : bool {
+    public function createUser() : bool {
         $sql = "INSERT INTO users (name, surname, username, password) 
                     values (:name, :surname, :username, :password)";
-        $stmt = $pdo->prepare($sql);
+        $stmt = $this->pdo->prepare($sql);
 
         $stmt->bindParam(":name", $this->name);
         $stmt->bindParam(":surname", $this->surname);
         $stmt->bindParam(":username", $this->username);
-        $stmt->bindParam(":password", $this->password);
+        $stmt->bindParam(":password",  password_hash($this->password, PASSWORD_BCRYPT));
 
         if ($stmt->execute()) {
             return true;
@@ -32,10 +29,29 @@ class User
         return false;
     }
 
-    public function isUsernameExists($pdo) : bool {
+    public function getUserData($id) {
+        $sql = "SELECT (name, surname, username, password) from users WHERE id = ?";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindParam(1, $id);
+        $stmt->execute();
+        $row = $stmt->fetch();
+
+        echo json_encode(
+            array (
+                "userId" => $row['id'],
+                "username" => $row['username'],
+                "name" => $row['name'],
+                "surname" => $row['surname'],
+            )
+        );
+    }
+
+    public function isUsernameExists() : bool {
         $sql = "SELECT username from users";
 
-        $stmt = $pdo->prepare($sql);
+        $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
 
         while ($row = $stmt->fetch()) {
